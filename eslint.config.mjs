b17d9751b2ccfc41a2870155b48786c14502332e
Success Runner <successrunner0222@gmail.com>
2025-06-06 12:04:47 -0400

    ignore unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,9 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "react-hooks/exhaustive-deps": "off"
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off"
     },
   },
 ];
